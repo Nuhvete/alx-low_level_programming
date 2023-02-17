7b702d011e6d3452dd92f56414db2a1cc234270e
Nuhvete <nuhvete22@gmail.com>
2023-02-17 07:54:57 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,6 +4,7 @@
  * main - A c program that prints a line to the standard error
  * Return: 1 (Successs)
  */
+
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
